afe52a848a914b701eb8c43360fcda607a1ad002
colinshenc <colinshenc@gmail.com>
2020-06-30 16:58:17 -0700

    new commit

--- BigGAN.py
+++ BigGAN.py
@@ -9,6 +9,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.nn import Parameter as P
 
+
 import layers
 from sync_batchnorm import SynchronizedBatchNorm2d as SyncBatchNorm2d
 
